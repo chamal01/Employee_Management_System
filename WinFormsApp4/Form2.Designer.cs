58d8a6bde1620698ea6296020c13274db4536c53
chamal adhikaram <100332002+chamal01@users.noreply.github.com>
2023-09-06 23:01:35 +0530

    Add files via upload

---
+++ WinFormsApp4/Form2.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace WinFormsApp4
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox4 = new TextBox();
+            textBox6 = new TextBox();
+            comboBox1 = new ComboBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            dateTimePicker1 = new DateTimePicker();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F);
+            label1.Location = new Point(309, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(203, 35);
+            label1.TabIndex = 0;
+            label1.Text = "Bsc/wd/21/31/10";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.Location = new Point(240, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(350, 32);
+            label2.TabIndex = 1;
+            label2.Text = "employee management system";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(53, 137);
+            label3.Name = "label3";
+            label3.Size = new Size(94, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Employee ID";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(53, 188);
+            label4.Name = "label4";
+            label4.Size = new Size(119, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Employee Name";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(53, 241);
+            label5.Name = "label5";
+            label5.Size = new Size(104, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Employee City";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(53, 431);
+            label6.Name = "label6";
+            label6.Size = new Size(58, 20);
+            label6.TabIndex = 5;
+            label6.Text = "contact";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(53, 383);
+            label7.Name = "label7";
+            label7.Size = new Size(92, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Joining Date";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(53, 338);
+            label8.Name = "label8";
+            label8.Size = new Size(32, 20);
+            label8.TabIndex = 7;
+            label8.Text = "Sex";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(53, 288);
+            label9.Name = "label9";
+            label9.Size = new Size(106, 20);
+            label9.TabIndex = 8;
+            label9.Text = "Employee Age";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(279, 134);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(364, 27);
+            textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(279, 181);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(364, 27);
+            textBox2.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(279, 285);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(364, 27);
+            textBox4.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(279, 424);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(364, 27);
+            textBox6.TabIndex = 14;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "kandy", "kundasale", "warapitiya", "peradeniya", "hanguranketha", "pilimathalawa", "theldeniya", "digana", "colombo", "nuwara-eliya", "mathale", "kurunagala", "mathara", "galle" });
+            comboBox1.Location = new Point(282, 241);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(361, 28);
+            comboBox1.TabIndex = 15;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(290, 334);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(63, 24);
+            radioButton1.TabIndex = 16;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Male";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(423, 334);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(78, 24);
+            radioButton2.TabIndex = 17;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Female";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(284, 382);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(359, 27);
+            dateTimePicker1.TabIndex = 18;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(387, 472);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 19;
+            button1.Text = "Insert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 585);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(848, 248);
+            dataGridView1.TabIndex = 20;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(308, 541);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 21;
+            button2.Text = "Update";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(472, 541);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 22;
+            button3.Text = "Delete";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(874, 845);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox4;
+        private TextBox textBox6;
+        private ComboBox comboBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private DateTimePicker dateTimePicker1;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ WinFormsApp4/Form2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp4
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection con = new SqlConnection("Data Source=ANONYMOUS;Initial Catalog=employee;Integrated Security=True");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //insert button
+
+            int empid = int.Parse(textBox1.Text);
+            string empname = textBox2.Text, city = comboBox1.Text, contact = textBox6.Text, sex = "";
+            double age = double.Parse(textBox4.Text);
+            DateTime joindate = DateTime.Parse(dateTimePicker1.Text);
+            if (radioButton1.Checked == true) { sex = "Male"; } else { sex = "Female"; }
+            con.Open();
+            SqlCommand c = new SqlCommand("exec Insert_emp '" + empid + "','" + empname + "','" + city + "','" + age + "','" + sex + "','" + joindate + "','" + contact + "'", con);
+            c.ExecuteNonQuery();
+            MessageBox.Show("Successful..!!");
+            GetEmpList();
+        }
+
+        void GetEmpList()
+        {
+            SqlCommand c = new SqlCommand("exec List_emp", con);
+            SqlDataAdapter sd = new SqlDataAdapter(c);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            GetEmpList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //update button
+            int empid = int.Parse(textBox1.Text);
+            string empname = textBox2.Text, city = comboBox1.Text, contact = textBox6.Text, sex = "";
+            double age = double.Parse(textBox4.Text);
+            DateTime joindate = DateTime.Parse(dateTimePicker1.Text);
+            if (radioButton1.Checked == true) { sex = "Male"; } else { sex = "Female"; }
+            con.Open();
+            SqlCommand c = new SqlCommand("exec Update_emp '" + empid + "','" + empname + "','" + city + "','" + age + "','" + sex + "','" + joindate + "','" + contact + "'", con);
+            c.ExecuteNonQuery();
+            MessageBox.Show("Successful Updated..!!");
+            GetEmpList();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //delete
+            if(MessageBox.Show("are you sure to delete that employer ?" ,"Delete Document",MessageBox.YesNo)==DialogResult.Yes)
+            {
+                int empid = int.Parse(textBox1.Text);
+                con.Open();
+                SqlCommand c = new SqlCommand("exec Delete_emp '" + empid + "'", con);
+                c.ExecuteNonQuery();
+                MessageBox.Show("Successful Deleted Employee..!!");
+                GetEmpList();
+            }
+        }
+            
+    }
+}
